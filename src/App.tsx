d6f2f62d005a66bcb1b249ce05be42827b126a86
Alexander <alexandertsyrkin@gmail.com>
2026-02-18 12:51:27 +0200

    Initial structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+    // Тут в будущем будет наш реестр (useState) и задачи (useEffect)
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default App
